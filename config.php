328eab348aae81d003a56dd4e7f859014b245c90
edenolam <julienbasquin.jb@gmail.com>
2022-04-09 19:10:14 +0200

    refacto config

--- config.php
+++ config.php
@@ -1,16 +1,10 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
+/* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-const DB_SERVER = 'localhost';
-const DB_USERNAME = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'crud';
-
-/* Attempt to connect to MySQL database */
-try {
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+try{
+    $pdo = new PDO("mysql:host=localhost;dbname=crud", "root", "");
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
+} catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
 }
